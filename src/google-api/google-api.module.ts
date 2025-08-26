fed004d2aacd7f5ee56d1e6ca0de5794275d9815
hani <hanikahhaleh@gmail.com>
2025-08-26 22:32:01 +0300

    remove httpservice related to axios

--- src/google-api/google-api.module.ts
+++ src/google-api/google-api.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GoogleApiService } from './google-api.service';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule],
   providers: [GoogleApiService],
   exports: [GoogleApiService],
 })
