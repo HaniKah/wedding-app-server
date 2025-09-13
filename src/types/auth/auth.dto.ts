6debd73d046d1fe30808a6723d721d868ab98527
hani <hanikahhaleh@gmail.com>
2025-09-14 00:14:18 +0200

    rename users dto to auth

--- src/types/auth/auth.dto.ts
+++ src/types/auth/auth.dto.ts
@@ -1,9 +1,13 @@
-export class UsersDto {
+export class SignUpDto {
   id: number;
   name: string;
   email: string;
   password: string;
   role: Role;
+}
+export class SignInDto {
+  email: string;
+  password: string;
 }
 export enum Role {
   Admin = 'Admin',
