090bc4f6c5e027d51a448964f421f6be4954d2f6
hani <hanikahhaleh@gmail.com>
2025-10-03 00:00:20 +0200

    add api property to coupleSide

--- src/types/guests/guests.dto.ts
+++ src/types/guests/guests.dto.ts
@@ -1,24 +1,29 @@
-export class GuestsDto {
-  id: number;
-  name: string;
-  phoneNumber: string;
-  coupleSide: CoupleSide;
-  isInvited: boolean;
-}
+import { ApiProperty } from '@nestjs/swagger';
 
 export enum CoupleSide {
   Groom = 'Groom',
   Bride = 'Bride',
 }
 
+export class GuestsDto {
+  id: number;
+  name: string;
+  phoneNumber: string;
+  @ApiProperty({ enum: CoupleSide, enumName: 'CoupleSide' })
+  coupleSide: CoupleSide;
+  isInvited: boolean;
+}
+
 export class AddGuestRequest {
   name: string;
   phoneNumber: string;
+  @ApiProperty({ enum: CoupleSide, enumName: 'CoupleSide' })
   coupleSide: CoupleSide;
 }
 
 export class UpdateGuestRequest {
   name?: string;
   phoneNumber?: string;
+  @ApiProperty({ enum: CoupleSide, enumName: 'CoupleSide' })
   coupleSide?: CoupleSide;
 }
