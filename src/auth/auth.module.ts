dddc6cba7b671991febe9d136d3aada2d5f8a514
hani <hanikahhaleh@gmail.com>
2025-10-12 11:43:45 +0200

    Revert "uncomment making global auth"
    This reverts commit 3e1b4105445e4c583c12e8cbb5ebd19fd6c3e661.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,21 +2,19 @@ import { Module } from '@nestjs/common';
 import { FirebaseService } from './firebase.service';
 import { FirebaseAuthGuard } from './firebase-auth.guard';
 import { AuthController } from './auth.controller';
-import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   controllers: [AuthController],
-  imports: [ConfigModule.forRoot()],
   providers: [
     FirebaseService,
     FirebaseAuthGuard,
-    // uncomment the following to apply Firebase auth globally to all routes
+    // Uncomment the following to apply Firebase auth globally to all routes
     // Routes can be made public using the @Public() decorator
-    {
-      provide: APP_GUARD,
-      useClass: FirebaseAuthGuard,
-    },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: FirebaseAuthGuard,
+    // },
   ],
   exports: [FirebaseService, FirebaseAuthGuard],
 })
