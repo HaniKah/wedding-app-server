b657bc757b5487c609b0c3c622e9cbf112e60d78
hani <hanikahhaleh@gmail.com>
2025-10-14 20:21:48 +0200

    remove changes concerning creating certificate

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as process from 'node:process';
-import * as fs from 'node:fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./cert/key.pem'),
-    cert: fs.readFileSync('./cert/cert.pem'),
-  };
-
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
 
