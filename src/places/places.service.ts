6ddd8a16e95614d80c90ec1dab870a878a4d7f21
hani <hanikahhaleh@gmail.com>
2025-12-02 23:24:05 +0100

    Revert "changes"
    This reverts commit d098727f46107ade3f2069e32e3eb82fc0aa1988.

--- src/places/places.service.ts
+++ src/places/places.service.ts
@@ -4,12 +4,12 @@ import {
   CreatePlaceSteps,
   PlacePrice,
   PlaceStatus,
+  UpdatePlaceLocation,
   UpdatePlaceRequest,
   VendorPlaceDetailsDto,
   VendorPlaceDetailsViewModel,
   VendorPlaceInfo,
   VendorPlaceListDto,
-  VendorPlaceLocation,
   VendorPlaceSocialMedia,
   VendorPlaceViewModel,
 } from '../types/places/places.dto';
@@ -192,7 +192,7 @@ export class PlacesService {
 
   private async updatePlaceLocation(
     placeId: number,
-    location: VendorPlaceLocation,
+    location: UpdatePlaceLocation,
   ) {
     return await this.placesRepositoryService.updatePlace(placeId, {
       country: location.country,
