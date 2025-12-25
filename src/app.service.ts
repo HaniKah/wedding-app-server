6b5318ea4db8fddc9f9f6da644670fa64cfa7007
hani <hanikahhaleh@gmail.com>
2025-12-25 20:45:51 +0100

    get hello testing

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log(`get hello reached on the ${process.env.NODE_ENV} environment`);
     return `Hello from the ${process.env.NODE_ENV} environment`;
   }
 }
