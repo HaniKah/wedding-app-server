ec523841b61fb037f03a450bd96ce1c9732bf383
hani <hanikahhaleh@gmail.com>
2026-03-27 14:52:25 +0100

    delete test exchange guard for now

--- src/auth/guards/exchange-auth/exchange-auth.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ExchangeAuthGuard } from './exchange-auth.guard';
-
-describe('ExchangeAuthGuard', () => {
-  it('should be defined', () => {
-    expect(new ExchangeAuthGuard()).toBeDefined();
-  });
-});
