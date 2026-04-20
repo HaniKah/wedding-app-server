02e44b9e4e04d659a790b1a199f326a37abf08b7
hani <hanikahhaleh@gmail.com>
2026-04-20 14:54:16 +0200

    delete migrations

--- migrations/migrations/2026-01-24-drop-currency.ts
+++
@@ -1,9 +0,0 @@
-import { Kysely } from 'kysely';
-
-export async function up(db: Kysely<any>): Promise<void> {
-  await db.schema
-    .withSchema('planner')
-    .alterTable('places')
-    .dropColumn('currency')
-    .execute();
-}
--- migrations/migrations/2026-01-25-add-ratio.ts
+++
@@ -1,9 +0,0 @@
-import { Kysely } from 'kysely';
-
-export async function up(db: Kysely<any>): Promise<void> {
-  await db.schema
-    .withSchema('planner')
-    .alterTable('photos')
-    .addColumn('ratio', 'double precision')
-    .execute();
-}
