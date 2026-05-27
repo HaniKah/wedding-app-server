b4c8fd9d01ebcf79de1acd3c8acc217eba531b26
hani <hanikahhaleh@gmail.com>
2026-05-27 13:57:07 +0200

    fix adding databse url

--- test/places.service.int-spec.ts
+++ test/places.service.int-spec.ts
@@ -39,10 +39,10 @@ describe('PlacesService (Integration)', () => {
     // Ensure we are using a test database
     const dbUrl = module
       .get<ConfigService>(ConfigService)
-      .get('DATABASE_URL_TEST');
-    if (!dbUrl || !dbUrl.includes('test')) {
+      .get('DATABASE_PUBLIC_URL');
+    if (!dbUrl || !dbUrl.includes('postgres_test')) {
       // Logic to prevent running against production if no test DB is set
-      // throw new Error('DATABASE_URL must be a test database');
+      throw new Error('DATABASE_PUBLIC_URL must be a test database');
     }
 
     // Clean up and Setup test user
