7dd9698c6573072c0e9d804807335aa255f81245
hani <hanikahhaleh@gmail.com>
2026-07-04 17:39:52 +0300

    change places order according to those who has photos

--- src/planner/planner.repository.service.ts
+++ src/planner/planner.repository.service.ts
@@ -18,7 +18,37 @@ export class PlannerRepositoryService {
 
     return await this.dbService.db
       .selectFrom('places')
-      .selectAll()
+      .selectAll('places')
+      .select((eb) =>
+        eb
+          .case()
+          .when(
+            eb.exists(
+              eb
+                .selectFrom('photos')
+                .select('photos.id')
+                .whereRef('photos.placeId', '=', 'places.id'),
+            ),
+          )
+          .then(1)
+          .else(0)
+          .end()
+          .as('hasPhotos'),
+      )
+      .select((eb) =>
+        eb
+          .case()
+          .when(
+            eb.and([
+              eb('places.promotionBeginsAt', '<=', now),
+              eb('places.promotionEndsAt', '>=', now),
+            ]),
+          )
+          .then(1)
+          .else(0)
+          .end()
+          .as('isPromoted'),
+      )
       .where('isPublished', '=', true)
       .where('places.deletedAt', 'is', null)
       .where('places.country', '=', countryCode)
@@ -34,20 +64,9 @@ export class PlannerRepositoryService {
       .$if(filters?.city !== undefined, (eb) =>
         eb.where('places.city', '=', filters.city),
       )
-      .orderBy((eb) =>
-        eb
-          .case()
-          .when(
-            eb.and([
-              eb('places.promotionBeginsAt', '<=', now),
-              eb('places.promotionEndsAt', '>=', now),
-            ]),
-          )
-          .then(0)
-          .else(1)
-          .end(),
-      )
       .orderBy('places.promotionBeginsAt', 'desc')
+      .orderBy('isPromoted', 'desc')
+      .orderBy('hasPhotos', 'desc')
       .orderBy('places.createdAt', 'desc')
       .limit(LIMIT)
       .offset(LIMIT * offset)
