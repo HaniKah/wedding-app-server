ba3bc98fc6274e3e9ff7bd02d7c34665304a403b
hani <hanikahhaleh@gmail.com>
2026-08-26 11:04:36 +0300

    add loggger to google

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -133,7 +133,7 @@ export class AuthController {
       exchangeToken,
       state: stateAndRedirectUriList[0],
     });
-    Logger.log('redirecting to :' + stateAndRedirectUriList[1]);
+    Logger.log('apple callback redirecting to :' + stateAndRedirectUriList[1]);
     return res.redirect(`${stateAndRedirectUriList[1]}?${params}`);
   }
 
@@ -153,6 +153,7 @@ export class AuthController {
       exchangeToken,
       state: stateAndRedirectUriList[0],
     });
+    Logger.log('google callback redirecting to :' + stateAndRedirectUriList[1]);
     return res.redirect(`${stateAndRedirectUriList[1]}?${params}`);
   }
 
